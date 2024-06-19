e43f1c83d5bdd7a4420faf6641293a1f85bbacbd
PRATHAM MISHRA <114295564+pratham99013@users.noreply.github.com>
2024-06-20 01:03:44 +0530

    Update settings.py

--- Thor/settings.py
+++ Thor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rnq=_6q)%!ete3o#e3x_o61o=&bw+!=r007w+@!4p2#yf8e6jy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
