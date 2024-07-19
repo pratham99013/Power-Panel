d7f5767d4c7b2b1f69a26be9082e426f0ef7792c
PRATHAM MISHRA <114295564+pratham99013@users.noreply.github.com>
2024-07-19 23:30:41 +0530

    Update settings.py

--- Thor/settings.py
+++ Thor/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 database_url = os.environ.get("DATABASE_URL")
-DATABASES['default'] = dj_database_url.parse("postgres://hello_0vlm_user:xLiX10DtOuXth2P9s4Mu2TC7LudynBgD@dpg-cphkmoocmk4c73ejsnc0-a.oregon-postgres.render.com/hello_0vlm")
+DATABASES['default'] = dj_database_url.parse("postgresql://pratham_y2ky_user:zsXMWXIgg1GFGQ6PEnItZ0XZ3gEQgDPH@dpg-cqdahp88fa8c73b24d80-a.oregon-postgres.render.com/pratham_y2ky")
 
 #postgres://hello_0vlm_user:xLiX10DtOuXth2P9s4Mu2TC7LudynBgD@dpg-cphkmoocmk4c73ejsnc0-a.oregon-postgres.render.com/hello_0vlm
 
